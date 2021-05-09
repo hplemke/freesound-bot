042370fcf7fb65148be6664d26c0e41184849a8c
Harrison Lemke <hplemke@gmail.com>
2021-05-09 16:19:34 -0500

    add retries in case of deleted sounds

--- freesound_bot.py
+++ freesound_bot.py
@@ -1,9 +1,19 @@
 import os
-import requests
 import random
+import requests
+from retry import retry
 import tweepy
 
 MAX_SOUND_ID = 570715
+
+# sometimes a sound has been deleted and we need to retry to get one that exists
+@retry(tries = 5)
+def get_random_sound_desc(token):
+    sound_id = random.randint(0, MAX_SOUND_ID)
+    response = requests.get(f'https://freesound.org/apiv2/sounds/{str(sound_id)}/?token={token}')
+    # raise an error if there was an error response code
+    response.raise_for_status()
+    return response.json()['description'][0:280]
 
 consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
 consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
@@ -13,11 +23,7 @@ freesound_client_secret = os.getenv('FREESOUND_CLIENT_SECRET')
 
 # get a random sound
 random.seed()
-sound_id = random.randint(0, MAX_SOUND_ID)
-response = requests.get(f'https://freesound.org/apiv2/sounds/{str(sound_id)}/?token={freesound_client_secret}')
-# raise an error if there was an error response code
-response.raise_for_status()
-tweet = response.json()['description'][0:280]
+tweet = get_random_sound_desc(freesound_client_secret)
 
 # post the sound description to twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
